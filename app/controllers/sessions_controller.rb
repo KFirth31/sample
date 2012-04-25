5dcf7c0d25b8ad0ccf8db0dfcd6a244131255003
Kyle Firth <KFirth31@gmail.com>
2012-04-25 15:41:46 -0400

    changed form_for to form_tag for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:session][:email].downcase)
+		user = User.find_by_email(params[:email].downcase)
 		flash.now[:error] = user
-	    if user && user.authenticate(params[:session][:password])
+	    if user && user.authenticate(params[:password])
 	      sign_in user
 	      redirect_to user
 	    else
